2584f904de3bd97b270ef49a0560fe9f0ebf85de
ge65waf <ge65waf@mytum.de>
2022-08-01 16:58:06 +0200

    backpack problem

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Leetcode
-Leetcode Storage
+Leetcode Storage  
 TODO LIST:
 - [ ] 背包问题的第二种方法(一维dp数组)
 - [ ] 动态规划问题392
+- [ ] 115_不同的子序列(困难)
 
--- 动态规划/392.py
+++ 动态规划/392.py
@@ -3,7 +3,7 @@ def isSubsequence(s, t):
     :type s: str
     :type t: str
     :rtype: bool
-    法一: 求最长公共子序列，同1143，然后比较最长公共子序列长度是否为s的长度
+    【法一】 求最长公共子序列，同1143，然后比较最长公共子序列长度是否为s的长度
     m, n = len(s), len(t)
     dp = [[0] * (n + 1) for i in range(m + 1)]
     for i in range(1, m + 1):
@@ -13,18 +13,34 @@ def isSubsequence(s, t):
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
     return dp[-1][-1] == m
-    法二:
-    建立dp，dp[i][j]表示在t中存在的
+
+    【法二】 以下，和1143有什么区别？？？
+
+    m, n = len(s), len(t)
+    dp = [[0] * (n + 1) for i in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    return dp[-1][-1] == m
+    法三: 双指针
     """
-    m, n = len(s), len(t)
-    dp = [[0] * (n + 1) for i in range(m + 1)]
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if s[i - 1] == t[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-    return dp[-1][-1] == m
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            j += 1
+    if i == len(s):
+        return True
+    else:
+        return False
+
+
+
 
 s = "abc"
 t = "ahbgdc"
